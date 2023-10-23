c88d5c3fafd515cb9f4ae59f97a4d50eb4042612
wangchaosun <2962056732@qq.com>
2023-10-23 20:56:33 +0800

    add udp

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main(){
         appBar: AppBar(
           title: const Center(child: Text("NanoClient"),),
         ),
-        body : const SwitchControlUI(),
+        body : SwitchControlUI(),
       )
     )
   );
--- lib/switch_udp_control.dart
+++ lib/switch_udp_control.dart
@@ -1,33 +1,52 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class SwitchCotrolCode {
+  static Uint8List stateCode =  Uint8List(2) ;
+  static int precode = 0xff;
+  static int oncode  = 0x01;
+  static int offcode = 0x00;
 
-  int precode = 0xff;
-  int oncode  = 0x01;
-  int offcode = 0x00;
-
-  SwitchCotrolCode(this.precode, this.oncode,this.offcode);
-
-  void getOnCode(){
-
+  static Uint8List getOnCode(){
+    stateCode[0] = 0xff;
+    stateCode[1] = oncode;
+    return stateCode;
   }
 
-  void getOffCode(){
-
+  static Uint8List getOffCode(){
+    stateCode[0] = 0xff;
+    stateCode[1] = offcode;
+    return stateCode;
   }
 
-  void getHelloCode(){
+  static void getHelloCode(){
     // 想要实现一个心跳信号
   }
   
 }
 
-
+// ignore: must_be_immutable
 class SwitchControlUI extends StatelessWidget {
-  const SwitchControlUI({ Key? key }) : super(key: key);
+  SwitchControlUI({ Key? key }) : super(key: key);
+
+  RawDatagramSocket? _socket; //? _socket可以为空  dart强制执行null检查 192.168.37.75
+  void startListening() async {  
+    _socket = await RawDatagramSocket.bind("0.0.0.0", 7777);
+  }
+
+  void sendMessage(Uint8List data) {
+    if (_socket == null) return;
+    int port = 6666;
+    InternetAddress address = InternetAddress("192.168.31.75");
+    _socket!.send(data, address, port);
+    
+  }
 
   @override
   Widget build(BuildContext context){
+    startListening();
     final Size size = MediaQuery.of(context).size;  
     return Container(
       padding: EdgeInsets.fromLTRB(0,  size.height/4, 0, 0),
@@ -35,15 +54,15 @@ class SwitchControlUI extends StatelessWidget {
       child: Column(   
         children: [
           SizedBox(  // turn on light
-            height: 60,
-            width: 100,
-            child: ElevatedButton.icon(onPressed: (){},icon: const Icon(Icons.light),label: const Text("on")),
+            height: 90,
+            width: 150,
+            child: ElevatedButton.icon(onPressed: (){sendMessage(SwitchCotrolCode.getOnCode());},icon: const Icon(Icons.light),label: const Text("on")),
           ),
           Container( // turn off light
             margin: const EdgeInsets.fromLTRB(0, 80, 0, 0),
-            height: 60,
-            width: 100,
-            child: ElevatedButton.icon(onPressed: (){},icon: const Icon(Icons.bedtime),label: const Text("off")),
+            height: 90,
+            width: 150,
+            child: ElevatedButton.icon(onPressed: (){sendMessage(SwitchCotrolCode.getOffCode());},icon: const Icon(Icons.bedtime),label: const Text("off")),
           ),
       ],
       ),
